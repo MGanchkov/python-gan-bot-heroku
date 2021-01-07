84cb7ec4a46f63e22a44260fb06982a9eb4e61cd
MGanchkov <56094035+MGanchkov@users.noreply.github.com>
2021-01-07 15:32:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -538,7 +538,7 @@ def start(update, context):
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    //update.message.reply_text('Help!')
+    # update.message.reply_text('Help!')
     update.message.send_message(helpText)
 
 def echo(update, context):
